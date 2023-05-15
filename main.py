66bcc7a8aed7fe51f39284d95a1ef14c19eba332
bulya <buleychenko29121998@gmail.com>
2023-05-15 21:12:06 +0200

    error

---
+++ main.py
@@ -0,0 +1,18 @@
+>>> len(3)
+TypeError: object of type 'int' has no len()
+
+>>> print(my_var)
+NameError: name 'my_var' is not defined
+
+>>> int('foo')
+ValueError: invalid literal for int() with base 10: 'foo'
+
+>>> my_dict = {'key1': 'value1', 'key2': 'value2'}
+>>> print(my_dict['key3'])
+KeyError: 'key3'
+
+>>> 1/0
+ZeroDivisionError: division by zero
+
+
+
